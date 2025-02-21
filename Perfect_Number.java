2603162a027e519f7bfb7dac70f1d75a35b805f9
DeepikaMahendran <167070954+deepikamahendran@users.noreply.github.com>
2025-02-21 21:57:49 +0530

    Create Perfect_Number.java

---
+++ Perfect_Number.java
@@ -0,0 +1,26 @@
+package demo;
+import java.util.Scanner;
+public class perfect_Number 
+{
+	    public static void main(String[] args) 
+	    {
+	        Scanner s = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        int num = s.nextInt();
+	        int sum = 0;
+
+	        for (int i = 1; i < num; i++) 
+	        {
+	            if (num % i == 0)
+	                sum += i;
+	        }
+	        if (sum == num)
+	        {
+	            System.out.println(num + " is a perfect number.");
+	        }
+	        else
+	        {
+	            System.out.println(num + " is not a perfect number.");
+	        }
+	    }
+	}
