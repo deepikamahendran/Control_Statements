e07e8b145baf5a8f35b7d7906f106522419af656
DeepikaMahendran <167070954+deepikamahendran@users.noreply.github.com>
2025-02-21 21:11:08 +0530

    Create Prime_number_if_else.java

---
+++ Prime_number_if_else.java
@@ -0,0 +1,37 @@
+package demo;
+import java.util.Scanner;
+public class PrimeNumber 
+{
+	    public static void main(String[] args) 
+	    {
+	        Scanner s = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        int num = s.nextInt();
+	        boolean isPrime = true;
+	        if (num <= 1) 
+	        {
+	            isPrime = false;
+	        }
+	        else 
+	        {
+	            for (int i = 2; i * i <= num; i++) 
+	            {
+	                if (num % i == 0) 
+	                {
+	                    isPrime = false;
+	                    break;
+	                }
+	            }
+	        }
+	        
+	        if (isPrime)
+	        {
+	            System.out.println(num + " is a prime number.");
+	        }
+	        else
+	        {
+	            System.out.println(num + " is not a prime number.");
+	        }
+	    }
+	}
+
